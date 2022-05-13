9ae3dc7ad4e5c77fd16f04ff00ebee3762311b76
sameerargadeinfy <sameer.argade1982@gmail.com>
2022-05-13 17:00:29 +1000

    Updated Index page
    Updated Index page

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
@@ -32,6 +33,7 @@ root.render(
         </Route>
       </Routes>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
